69d843e89a71b00b6ffddd1f29a53911d54b6056
Reuven <dagagareuven@gmail.com>
2024-12-24 16:12:11 +0200

    כ

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';  
-          }
-        }
-      }
-    }
+    chunkSizeWarningLimit: 1000  
   }
 });
